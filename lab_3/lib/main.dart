7ab9eb042106d2852eab3896a7f030e7379e8449
philjones <pchukwuemeke407@gmail.com>
2025-03-24 22:16:00 +0300

    Added decrement and reset buttons.

--- lab_3/lib/main.dart
+++ lab_3/lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Инкремент'),
     );
   }
 }
@@ -64,6 +64,18 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+    void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+    void _resetCounter() {
+    setState(() {
+      _counter = 0;
     });
   }
 
@@ -104,19 +116,57 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Значение инкремента:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: _decrementCounter, 
+                  icon: const Icon(Icons.remove),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
+                    fixedSize: const Size(50, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20,),
+                IconButton(
+                  onPressed: _incrementCounter, 
+                  icon: const Icon(Icons.add), 
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    fixedSize: const Size(50, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20,),
+            TextButton(
+              onPressed: _resetCounter, 
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.grey,
+              ),
+              child: const Text('Сбросить'), 
+            
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      /*floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),*/ // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
